2a75a19e64f69497d40435634a7be31b25d1e46d
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-01-13 17:39:53 +0100

    forms component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,39 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { FormsComponent } from './components/forms/forms.component';
+import { CercaComponent } from './components/cerca/cerca.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { CommerceStatsComponent } from './components/commerce-stats/commerce-stats.component';
+import { NotFoundComponent } from './shared/not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{
+path:'',
+component:FormsComponent
+},
+{
+path:'forms',
+component:FormsComponent
+},
+{
+path:'cerca',
+component:CercaComponent
+},
+{
+path:'dashboard',
+component:DashboardComponent
+},
+{
+path:'stats',
+component:CommerceStatsComponent
+},
+{
+  path:'**',
+  component:NotFoundComponent
+}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/components/forms/forms.component.ts
+++ src/app/components/forms/forms.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-forms',
   templateUrl: './forms.component.html',
   styleUrls: ['./forms.component.scss']
 })
-export class FormsComponent {
+export class FormsComponent implements OnInit{
 
+login!:FormGroup
+formSection:string=''
+signup!:FormGroup
+ngOnInit(): void {
+this.login=new FormGroup({
+email:new FormControl('',[Validators.required,Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]),
+password:new FormControl('',Validators.required)
+})
+this.formSection='login'
+this.signup=new FormGroup({
+  nomeCompleto: new FormControl('',[Validators.required,Validators.minLength(2)]),
+  email:new FormControl('',[Validators.required,Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]),
+  nazione:new FormControl('',Validators.required),
+  città:new FormControl('',Validators.required),
+  età:new FormControl('',[Validators.required,Validators.min(18)]),
+password:new FormControl('',Validators.required),
+ripetiPassword:new FormControl('',Validators.required)
+})
+}
 }
