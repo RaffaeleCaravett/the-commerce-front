1dd65084b0db37c1795aea8ce686e06b78eb6c4f
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-01-13 15:55:13 +0100

    installed material, bootstrap and toastr

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,39 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsComponent } from './components/forms/forms.component';
+import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { CercaComponent } from './components/cerca/cerca.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { ViewProdottoComponent } from './shared/view-prodotto/view-prodotto.component';
+import { ErrorComponent } from './shared/error/error.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormsComponent,
+    LoginComponent,
+    HomeComponent,
+    SignupComponent,
+    DashboardComponent,
+    CercaComponent,
+    NavbarComponent,
+    FooterComponent,
+    ViewProdottoComponent,
+    ErrorComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot(),
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/cerca/cerca.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cerca',
+  templateUrl: './cerca.component.html',
+  styleUrls: ['./cerca.component.scss']
+})
+export class CercaComponent {
+
+}
