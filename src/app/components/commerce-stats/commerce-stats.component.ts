734084bea2c518b2e63dadd69755a742214baf9e
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-01-14 09:37:36 +0100

    added e charts

--- src/app/components/commerce-stats/commerce-stats.component.ts
+++ src/app/components/commerce-stats/commerce-stats.component.ts
@@ -8,8 +8,9 @@ import { Component, OnInit } from '@angular/core';
 export class CommerceStatsComponent implements OnInit{
 
   chartOptions: any
-
-
+option:any
+opt:any
+series:any
   ngOnInit(): void {
     this.chartOptions = {
       // Your ECharts configuration goes here
@@ -25,6 +26,133 @@ export class CommerceStatsComponent implements OnInit{
         type: 'bar'
       }]
     };
+
+    this.option = {
+      legend: {},
+      tooltip: {
+        trigger: 'axis',
+        showContent: false
+      },
+      dataset: {
+        source: [
+          ['product', '2012', '2013', '2014', '2015', '2016', '2017'],
+          ['Milk Tea', 56.5, 82.1, 88.7, 70.1, 53.4, 85.1],
+          ['Matcha Latte', 51.1, 51.4, 55.1, 53.3, 73.8, 68.7],
+          ['Cheese Cocoa', 40.1, 62.2, 69.5, 36.4, 45.2, 32.5],
+          ['Walnut Brownie', 25.2, 37.1, 41.2, 18, 33.9, 49.1]
+        ]
+      },
+      xAxis: { type: 'category' },
+      yAxis: { gridIndex: 0 },
+      grid: { top: '55%' },
+      series: [
+        {
+          type: 'line',
+          smooth: true,
+          seriesLayoutBy: 'row',
+          emphasis: { focus: 'series' }
+        },
+        {
+          type: 'line',
+          smooth: true,
+          seriesLayoutBy: 'row',
+          emphasis: { focus: 'series' }
+        },
+        {
+          type: 'line',
+          smooth: true,
+          seriesLayoutBy: 'row',
+          emphasis: { focus: 'series' }
+        },
+        {
+          type: 'line',
+          smooth: true,
+          seriesLayoutBy: 'row',
+          emphasis: { focus: 'series' }
+        },
+        {
+          type: 'pie',
+          id: 'pie',
+          radius: '30%',
+          center: ['50%', '25%'],
+          emphasis: {
+            focus: 'self'
+          },
+          label: {
+            formatter: '{b}: {@2012} ({d}%)'
+          },
+          encode: {
+            itemName: 'product',
+            value: '2012',
+            tooltip: '2012'
+          }
+        }
+      ]
  }
+ this.opt = {
+  legend: {
+    top: 'bottom'
+  },
+  toolbox: {
+    show: true,
+    feature: {
+      mark: { show: true },
+      dataView: { show: true, readOnly: false },
+      restore: { show: true },
+      saveAsImage: { show: true }
+    }
+  },
+  series: [
+    {
+      name: 'Nightingale Chart',
+      type: 'pie',
+      radius: [50, 250],
+      center: ['50%', '50%'],
+      roseType: 'area',
+      itemStyle: {
+        borderRadius: 8
+      },
+      data: [
+        { value: 40, name: 'rose 1' },
+        { value: 38, name: 'rose 2' },
+        { value: 32, name: 'rose 3' },
+        { value: 30, name: 'rose 4' },
+        { value: 28, name: 'rose 5' },
+        { value: 26, name: 'rose 6' },
+        { value: 22, name: 'rose 7' },
+        { value: 18, name: 'rose 8' }
+      ]
+    }
+  ]
+}
+this.series  = {
+  xAxis: {
+    type: 'category',
+    data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+  },
+  yAxis: {
+    type: 'value'
+  },
+  series: [
+    {
+      data: [
+        120,
+        {
+          value: 200,
+          itemStyle: {
+            color: '#a90000'
+          }
+        },
+        150,
+        80,
+        70,
+        110,
+        130
+      ],
+      type: 'bar'
+    }
+  ]
+}
+}
 
 }
