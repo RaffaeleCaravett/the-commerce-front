948a469b48086ee18c732c2fcaf56f84e1c9781f
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-01-17 12:12:08 +0100

    preparing to post categories and get them from server

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
-
+import { Component, OnDestroy, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit,OnDestroy{
+
+images:string[]=[]
+index:number=0
+interval:any
+constructor(){}
+  ngOnDestroy(): void {
+clearInterval(this.interval)  }
+
+ngOnInit(): void {
+  let image= '../../../assets/homeImages/banner.jpg'
+  let image1= '../../../assets/homeImages/banner1.jpg'
+  let image2= '../../../assets/homeImages/banner2.jpg'
+  this.images.push(image,image1,image2)
+
+  this.interval= setInterval(()=>{
+if(this.index==2){
+  this.index=0
+}else{
+  this.index+=1
+}
+  },5000)
+}
+updateCarousel(direction:string){
+if(direction=='back'){
+  if(this.index==0){
+    this.index=2
+  }else{
+    this.index-=1
+  }
+}else{
+  if(this.index==2){
+    this.index=0
+  }else{
+    this.index+=1
+  }
+}
+}
+
 
 }
