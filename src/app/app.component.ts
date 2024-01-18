648e90a17eb6a4ad447fc8fc48593edea00732a2
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-01-18 16:45:13 +0100

    refresh token works well

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AppService } from './services/app.service';
 import { AuthService } from './services/auth.service';
+import { JsonPipe } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -10,7 +12,7 @@ import { AuthService } from './services/auth.service';
 export class AppComponent implements OnInit{
   title = 'theCommerce';
 
-  constructor(private app:AppService,private authService:AuthService){}
+  constructor(private app:AppService,private authService:AuthService,private router:Router){}
 
   ngOnInit(){
 this.app.saveVisit().subscribe((data:any)=>{})
@@ -18,18 +20,27 @@ this.app.saveVisit().subscribe((data:any)=>{})
 
 if(localStorage.getItem('authToken')){
   this.authService.verifyToken(localStorage.getItem('authToken')!).subscribe((data:any)=>{
-    console.log(data)
-//      this.authService.setToken(data.accessToken)
-// this.authService.setRefreshToken(data.refreshToken)
-// this.authService.authenticateUser(true)
-// localStorage.setItem('authToken',this.authService.token)
-// localStorage.setItem('refreshToken',this.authService.token)
-// this.router.navigate(['/home'])
+    if(data){
+      localStorage.setItem('user',JSON.stringify(data))
+      this.authService.setToken(localStorage.getItem('authToken')!)
+      this.authService.setRefreshToken(localStorage.getItem('refreshToken')!)
+      this.authService.authenticateUser(true)
+      localStorage.setItem('authToken',this.authService.token)
+      localStorage.setItem('refreshToken',this.authService.token)
+      this.router.navigate(['/home'])
+    }
+
   },err=>{
     if(localStorage.getItem('refreshToken')){
       this.authService.verifyRefreshToken(localStorage.getItem('refreshToken')!).subscribe((data:any)=>{
-console.log(data)
-      })
+ if(data){
+      this.authService.setToken(data.tokens.accessToken)
+      this.authService.setRefreshToken(data.tokens.refreshToken)
+      this.authService.authenticateUser(true)
+      localStorage.setItem('authToken',this.authService.token)
+      localStorage.setItem('refreshToken',this.authService.token)
+      this.router.navigate(['/home'])
+    }      })
     }
 
   })
